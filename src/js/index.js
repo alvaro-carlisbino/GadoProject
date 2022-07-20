98e3721c50ea6bc6c972c4b93affd090b4b50dc8
Bryzzen <xdbrgames10@gmail.com>
2022-07-20 20:35:15 -0300

    Para ela...

--- src/js/index.js
+++ src/js/index.js
@@ -31,25 +31,35 @@ function load() {
           console.log(time);
           if (time.isConfirmed) {
             new Swal({
-                title: "",
-                text: "Eu te amo para sem sempre",
-                icon: "success",
-                timer: 3000,
-                timerProgressBar: true,
-            }).then((_) => {
-                window.location.href = "https://memegenerator.net/img/instances/82001237/eu-te-amo-muito.jpg"
-            })
-          } else {
-            new Swal({
               title: "",
-              text: "Você não me quer? :(",
-              icon: "error",
+              text: "Eu te amo para sem sempre",
+              icon: "success",
               timer: 3000,
               timerProgressBar: true,
-            }).then((a) => {
-              if (a) {
-                load();
-              }
+            }).then((_) => {
+              window.location.href =
+                "https://memegenerator.net/img/instances/82001237/eu-te-amo-muito.jpg";
+            });
+          } else {
+            document.getElementById(
+              "switch"
+            ).innerHTML = `<input type="checkbox" onclick="load()"/>
+            <span class="slider"></span>`;
+            const Toast = Swal.mixin({
+              toast: true,
+              position: "top-end",
+              showConfirmButton: false,
+              timer: 3000,
+              timerProgressBar: true,
+              didOpen: (toast) => {
+                toast.addEventListener("mouseenter", Swal.stopTimer);
+                toast.addEventListener("mouseleave", Swal.resumeTimer);
+              },
+            });
+
+            Toast.fire({
+              icon: "error",
+              title: "Pensei que você me amava...",
             });
           }
         });
